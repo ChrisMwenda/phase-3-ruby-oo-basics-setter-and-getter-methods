25c055f8e9141fe6530b5438e878ba8802132b22
Christopher <chrismbogo97@gmail.com>
2022-10-24 18:12:18 +0300

    Completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
+require 'pry'
 class Dog
+    def name=(dog_name)
+        @name=dog_name
+    end
+    def name
+        @name
+    end
+
+    def breed=(dog_breed)
+        @breed=dog_breed
+    end
+    def breed
+        @breed
+    end
 
 end
+
+Bruno=Dog.new
+Bruno.name="Bruno"
+Bruno.breed="Rottweiler"
+# binding.pry
+# 0
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
-
+def name=(person_name)
+    @name=person_name
 end
+def name
+    @name
+end
+
+def job=(person_job)
+    @job=person_job
+end
+def job
+    @job
+end
+end
+
+Chris=Person.new
+Chris.name="Chris"
+Chris.job="software developer"
